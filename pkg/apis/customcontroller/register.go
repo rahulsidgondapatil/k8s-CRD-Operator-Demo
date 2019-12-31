c87801a2737b73a60c6ad6c7758f98eeffcf23e2
rahulsidgondapatil <rahulsidgondapatil@gmail.com>
2019-12-31 12:16:46 +0530

    revamp

---
+++ pkg/apis/customcontroller/register.go
@@ -0,0 +1,6 @@
+package customcontroller
+
+//GroupName is the group name used in this package
+const (
+	GroupName = "customcontroller.k8s.io"
+)
---
+++ pkg/apis/customcontroller/v1alpha1/doc.go
@@ -0,0 +1,6 @@
+// +k8s:deepcopy-gen=package
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=customcontroller.k8s.io
+
+// Package v1alpha1 is the v1alpha1 version of the API.
+package v1alpha1 // import "github.com/rahulsidgondapatil/sample-customController/pkg/apis/customcontroller/v1alpha1"
